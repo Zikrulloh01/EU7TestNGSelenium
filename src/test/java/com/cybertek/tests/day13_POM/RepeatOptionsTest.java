e7a152818a45fb3ea3442f69fa9235fa647fa446
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-01-24 12:35:16 +0000

    day13 RepeatOptionsTest has been tested

--- src/test/java/com/cybertek/tests/day13_POM/RepeatOptionsTest.java
+++ src/test/java/com/cybertek/tests/day13_POM/RepeatOptionsTest.java
@@ -1,13 +1,27 @@
 package com.cybertek.tests.day13_POM;
 
+import com.cybertek.pages.*;
 import com.cybertek.tests.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RepeatOptionsTest extends TestBase {
 
+    LoginPage loginPage = new LoginPage();
+    BasePage dashboardPage = new DashboardPage();
+    CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+    CreateCalendarEventsPage createCalendarEventsPage = new CreateCalendarEventsPage();
+
     @Test
     public void test1(){
-
+        loginPage.loginAsDriver();
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+        calendarEventsPage.waitUntilLoaderScreenDisappear();
+        calendarEventsPage.createCalendarEvent.click();
+        createCalendarEventsPage.waitUntilLoaderScreenDisappear();
+        createCalendarEventsPage.repeat.click();
+        Assert.assertTrue(createCalendarEventsPage.repeat.isSelected());
+        Assert.assertFalse(createCalendarEventsPage.weekday.isSelected());
     }
 
 }
