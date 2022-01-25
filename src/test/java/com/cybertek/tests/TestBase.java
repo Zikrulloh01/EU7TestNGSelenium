244e98074adb1852426cb6e9bb3df355382c99f9
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2022-01-25 10:20:07 +0000

    TestBase updated

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -2,12 +2,17 @@ package com.cybertek.tests;
 
 import static com.cybertek.utilities.Driver.*;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cybertek.utilities.ConfigurationReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +21,27 @@ public class TestBase {
     protected WebDriverWait wait;
     private Actions actions;
 
+
+    protected ExtentReports report;
+    protected ExtentHtmlReporter htmlReporter;
+    protected ExtentTest extentLogger;
+
+    @BeforeTest
+    public void setUpTest(){
+        report = new ExtentReports();
+
+        String projectPath = System.getProperty("user.dir");
+        String path = projectPath + "/test-output/report.html";
+        htmlReporter = new ExtentHtmlReporter(path);
+
+
+        report.attachReporter(htmlReporter);
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
+
+        report.setSystemInfo("Environment", "QA");
+        report.setSystemInfo("Browser", ConfigurationReader.getKey("browser"));
+        report.setSystemInfo("OS", System.getProperty("os.name"));
+    }
 
     @BeforeMethod
     public void setUp(){
@@ -33,4 +59,10 @@ public class TestBase {
         Thread.sleep(2000);
         closeDriver();
     }
+
+    @AfterTest
+    public void tearDownTest(){
+        report.flush();
+    }
+
 }
